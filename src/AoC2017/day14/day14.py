edc977de8d6328f6c6512eb62d3107ce6350a045
Yachim <jachym.kohout@gmail.com>
2022-07-03 23:40:18 +0200

    Add day 15

--- src/AoC2017/day14/day14.py
+++ src/AoC2017/day14/day14.py
@@ -1,29 +1,56 @@
 import sys
-from venv import create
 
 if __name__ == "__main__":
     sys.path.append("../../..")
-from src.AoC2017.day10.day10 import part2
+from src.AoC2017.day10.day10 import part2 as knot_h
+
 
 def get_binary(inp: str):
-    return bin(int(inp, 16))[2:].zfill(4)
+    return "".join([bin(int(i, 16))[2:].zfill(4) for i in inp])
 
 def create_grid(inp: str):
     grid = []
     for i in range(128):
-        knot = part2(256, f"{inp}-{i}") 
+        knot = knot_h(256, f"{inp}-{i}") 
         b = get_binary(knot)
-        grid.append(bool(int(i)) for i in b)
+        grid.append([bool(int(i)) for i in b])
 
     return grid
+
+def update_cell(grid: list, line_index: int, cell_index: int):
+    try:
+        if not grid[line_index][cell_index]: return
+    except:
+        print()
+    grid[line_index][cell_index] = False
+    
+
+    if line_index > 0: update_cell(grid, line_index - 1, cell_index)
+    if line_index < len(grid) - 1: update_cell(grid, line_index + 1, cell_index)
+    if cell_index > 0: update_cell(grid, line_index, cell_index - 1)
+    if cell_index < len(grid[line_index]) - 1: update_cell(grid, line_index, cell_index + 1)
+
 
 def part1(inp: str):
     grid = create_grid(inp)
     return sum(sum(i) for i in grid)
+
+def part2(inp: str):
+    grid = create_grid(inp).copy()
+    total = 0
+    for line_index, line in enumerate(grid):
+        for cell_index, cell in enumerate(line):
+            if not cell: continue
+            update_cell(grid, line_index, cell_index)
+            total += 1
+
+    return total
+
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
         inp = f.read()
 
     print(part1(inp))
+    print(part2(inp))
     
--- tests/AoC2017/day14/test_day14.py
+++ tests/AoC2017/day14/test_day14.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import unittest
+from unittest.mock import patch
 
 from src.AoC2017.day14.day14 import *
 
@@ -9,6 +10,59 @@ class TestBinary(unittest.TestCase):
     def test_binary(self):
         self.assertEqual(get_binary("a0c2017"), "1010000011000010000000010111")
 
+class TestCellUpdate(unittest.TestCase):
+    def test1(self):
+        grid = [
+            [True, True, False],
+            [False, True, False],
+            [False, False, True]
+        ]
+        update_cell(grid, 1, 1)
+
+        self.assertEqual(grid, [
+            [False, False, False],
+            [False, False, False],
+            [False, False, True]
+        ])
+    def test2(self):
+        grid = [
+            [True, True, False],
+            [False, False, False],
+            [False, True, True]
+        ]
+        update_cell(grid, 1, 1)
+
+        self.assertEqual(grid, [
+            [True, True, False],
+            [False, False, False],
+            [False, True, True]
+        ])
+    def test3(self):
+        grid = [
+            [True, True, False],
+            [False, True, False],
+            [False, True, True]
+        ]
+        update_cell(grid, 0, 1)
+
+        self.assertEqual(grid, [
+            [False, False, False],
+            [False, False, False],
+            [False, False, False]
+        ])
+
 class TestPart1(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1("flqrgnkx"), 8108)
+
+class TestPart2(unittest.TestCase):
+    @patch("src.AoC2017.day14.day14.create_grid", return_value=[
+        [True, True, False],
+        [True, False, False],
+        [False, False, True]
+    ])
+    def test_custom(self, mocked):
+        self.assertEqual(part2(""), 2)
+
+    def test_part2(self):
+        self.assertEqual(part2("flqrgnkx"), 1242)
