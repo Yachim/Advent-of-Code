e30fad4475a61e3d3b2051227db854c5de153347
Yachim <jachym.kohout@gmail.com>
2022-08-07 23:00:45 +0200

    New day changes

--- new_day.py
+++ new_day.py
@@ -5,12 +5,12 @@ from venv import create
 
 def process_input(inp: str) -> Tuple[str, str]:
     inp = [int(i.strip()) for i in inp.split(";")]
-    if len(inp) != 2: raise Exception("Špatný formát vstupu")
+    if len(inp) != 2: raise Exception("Bad input format")
     [year, day] = inp
-    if year < 2015: raise Exception("Moc nízký rok")
-    if year > 2021: raise Exception("Moc vysoký rok")
-    if day < 1: raise Exception("Moc nízký den")
-    if day > 25: raise Exception("Moc vysoký den")
+    if year < 2015: raise Exception("Year too low")
+    if year > 2021: raise Exception("Year too high")
+    if day < 1: raise Exception("Day too low")
+    if day > 25: raise Exception("Day too high")
 
     return (f"AoC{year}", f"day{str(day).zfill(2)}")
 
@@ -23,17 +23,15 @@ def folder_empty(folder_name: str) -> bool:
 
 def create_folder(path: str):
     mkdir(path)
-    f = open(f"{path}/__init__.py", "x")
-    f.close()
 
 def create_files(path: str, day: str, name_pref=""):
-    f = open(f"{path}/__init__.py", "x")
     f = open(f"{path}/{name_pref}{day}.py", "x")
     f = open(f"{path}/{name_pref}input.txt", "x")
+    f = open(f"{path}/__init__.py", "x")
     f.close()
 
 if __name__ == "__main__":
-    [year, day] = process_input(input("Formát: [číslo roku];[číslo dne], např. 2019;1\n"))
+    [year, day] = process_input(input("Format: [year];[day], ie. 2019;1\n"))
 
     if not folder_exists(year):
         create_folder(f"src/{year}")
@@ -48,10 +46,33 @@ if __name__ == "__main__":
         create_files(f"tests/{year}/{day}", day, "test_")
 
         with open(f"tests/{year}/{day}/test_{day}.py", "w") as f:
-            f.write(f"import unittest\n\nfrom src.{year}.{day}.{day} import *")
+            f.write(f"""import unittest
+            
+from src.{year}.{day}.{day} import *
+
+
+class TestInputProcessing(unittest.TestCase):
+    def test(self):
+        self.assertEqual(process_input(), None)
+        
+class TestPart1(unittest.TestCase):
+    def test(self):
+        self.assertEqual(part1(), None)
+        
+class TestPart2(unittest.TestCase):
+    def test(self):
+        self.assertEqual(part2(), None)""")
 
         with open(f"src/{year}/{day}/{day}.py", "w") as f:
             f.write('''
+def process_input(raw_in):
+    pass
+
+def part1(inp):
+    pass
+
+def part2(inp):
+    pass
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
